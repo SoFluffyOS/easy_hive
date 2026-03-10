5426088756240f2f01d0cb19365e1bd6cfe1ff53
Simon Pham <simonpham.dn@gmail.com>
2026-03-10 22:04:49 +0700

    🧪 Add regression test for EasyBox initialization failure

--- packages/easy_hive/test/src/easy_hive_test.dart
+++ packages/easy_hive/test/src/easy_hive_test.dart
@@ -1,7 +1,27 @@
-// ignore_for_file: prefer_const_constructors
-
+import 'package:easy_hive/src/easy_box.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+class MockEasyBox extends EasyBox<dynamic> {
+  @override
+  String get boxKey => 'mock_box';
+
+  @override
+  bool get isEncrypted => true;
+
+  @override
+  Future<void> openEncryptedBox() async {
+    throw Exception('Failed to open encrypted box');
+  }
+
+  @override
+  Future<void> openNormalBox() async {}
+}
+
 void main() {
-  group('EasyHive', () {});
+  group('EasyBox', () {
+    test('init() should propagate exceptions from openEncryptedBox', () async {
+      final box = MockEasyBox();
+      expect(() => box.init(), throwsException);
+    });
+  });
 }
