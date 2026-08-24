4b8f7362bf3e1e22512e63c38e1a951c67faa654
Simon Pham <simonpham.dn@gmail.com>
2026-08-24 10:27:43 +0700

    🩹 Retry upon open encrypted box failure for iOS

--- packages/easy_hive_encryption/lib/src/mixin/encryption_mixin.dart
+++ packages/easy_hive_encryption/lib/src/mixin/encryption_mixin.dart
@@ -1,46 +1,63 @@
 // ignore_for_file: depend_on_referenced_packages
 
 import 'dart:convert';
+import 'dart:developer' as dev;
 
 import 'package:easy_hive/easy_hive.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:hive/hive.dart';
 
 mixin EncryptionMixin<T> on EasyBox<T> {
+  static const Duration _retryDelayDuration = Duration(seconds: 1);
+
   @override
   bool get isEncrypted => true;
 
   @override
   Future<void> openEncryptedBox() async {
-    const secureStorage = FlutterSecureStorage(
-      aOptions: AndroidOptions(
-        encryptedSharedPreferences: true,
-        resetOnError: true,
-        keyCipherAlgorithm:
-            KeyCipherAlgorithm.RSA_ECB_OAEPwithSHA_256andMGF1Padding,
-        storageCipherAlgorithm: StorageCipherAlgorithm.AES_GCM_NoPadding,
-      ),
-    );
-    var key = await secureStorage.read(key: encryptionKeyName);
-    if (key == null || key.isEmpty) {
-      key = base64UrlEncode(Hive.generateSecureKey());
-      await secureStorage.write(
-        key: encryptionKeyName,
-        value: key,
+    try {
+      const secureStorage = FlutterSecureStorage(
+        aOptions: AndroidOptions(
+          encryptedSharedPreferences: true,
+          resetOnError: true,
+          keyCipherAlgorithm:
+              KeyCipherAlgorithm.RSA_ECB_OAEPwithSHA_256andMGF1Padding,
+          storageCipherAlgorithm: StorageCipherAlgorithm.AES_GCM_NoPadding,
+        ),
       );
-    }
-    final encryptionKey = base64Url.decode(key);
+      var key = await secureStorage.read(key: encryptionKeyName);
+      if (key == null || key.isEmpty) {
+        key = base64UrlEncode(Hive.generateSecureKey());
+        await secureStorage.write(
+          key: encryptionKeyName,
+          value: key,
+        );
+      }
+      final encryptionKey = base64Url.decode(key);
 
-    if (isLazy) {
-      box = await Hive.openLazyBox(
-        boxKey,
-        encryptionCipher: HiveAesCipher(encryptionKey),
-      );
-    } else {
-      box = await Hive.openBox(
-        boxKey,
-        encryptionCipher: HiveAesCipher(encryptionKey),
-      );
+      if (isLazy) {
+        box = await Hive.openLazyBox(
+          boxKey,
+          encryptionCipher: HiveAesCipher(encryptionKey),
+        );
+      } else {
+        box = await Hive.openBox(
+          boxKey,
+          encryptionCipher: HiveAesCipher(encryptionKey),
+        );
+      }
+    } catch (err, trace) {
+      if (kDebugMode) {
+        dev.log(
+          '$err',
+          name: 'EncryptionMixin<${T.runtimeType}>',
+          stackTrace: trace,
+        );
+      }
+      // Retry to prevent failure when iOS is in lockscreen.
+      await Future.delayed(_retryDelayDuration);
+      await openEncryptedBox();
     }
   }
 }
